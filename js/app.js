f80f036e83f570068bc43265ad3cb405317ecabf
Ron Yu <ronyu77@gmail.com>
2014-12-15 14:30:13 +0800

    1.04 nav item auto-disappear is done.
    Why:
    In order to improve UX when user clicks nag items and window scrolls
    down in small size device.
    How:
     1. find the threshold width for small device in media query.css (768px)
    2. find window width($(window).width())
    3. set up condition(if) to make nav item disappear  after scrolling.
    What:
    when user click nag items in small device, the nav items will slide up
    after clicking. When user click nag items in big device, everything
    keeps originnal.

--- js/app.js
+++ js/app.js
@@ -25,11 +25,15 @@ $(document).ready(function () {
        var $this = $(this);
        var $linkItem = $this.attr("href");
        var $linkSection = $($linkItem);
+       var $winWidth = $(window).width();
       $("html, body").animate({
         scrollTop: $($linkSection).offset().top-60
       },
       800
        );
+       if($winWidth < 768) {
+         $("#nav").slideUp(1000);
+       }
        event.preventDefault();
      });
      //because of fixed nav bar, you have to make the coordinated section not to hide by it
